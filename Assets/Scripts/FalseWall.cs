a3a2a9c1eb6b20108dc6fc05aeb7801078bd5659
IDKWIAm <133805972+IDKWIAm@users.noreply.github.com>
2024-05-11 00:07:48 +0300

    Alpha 2
    • Геймплей:
    ---Возвращена механика открытия секреток при касании.
    
    • Окружение:
    ---Новые спрайты окружения и врага.
    ---Добавлен задний фон и может иметь эффект глубины.
    
    • UI:
    ---Добавлен визуал главного меню: шрифт и несколько спрайтов.
    
    • Другое:
    ---Префаб grid сэмпла обновлен.

---
+++ Assets/Scripts/FalseWall.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class FalseWall : MonoBehaviour
+{
+    Animator _animator;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 11)
+        {
+            _animator.SetTrigger("Triggered");
+        }
+    }
+}
